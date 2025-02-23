2a1b12f6d2a270b8371cc616dec960da5b59610a
Siddhant M <meetsiddhant@gmail.com>
2025-02-22 21:31:03 -0500

    Added sticky navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
         >
           <main className="min-h-screen flex flex-col items-center">
             <div className="flex-1 w-full flex flex-col gap-20 items-center">
-              <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+              <nav className="w-full fixed top-0 z-50 bg-black text-white shadow-md flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
                     <Link href={"/"}>Stunite - Make friends you actually like</Link>
